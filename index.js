5643cb4db148d1c64f9efd024e8661d691eb3d90
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2023-06-20 10:19:13 +0100

    changed port setting

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+//const port = 3000
+const port = process.env.PORT || 3000
 const cors = require('cors');
 var path = require('path');
 const pgPool = require('./dbConfig');
