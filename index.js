27322ca24d16194133d967ea45093c9e83f39e39
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2023-06-21 13:00:03 +0100

    port tweak reintroduced

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 10000
+const port = process.env.PORT || 3000
 const cors = require('cors');
 var path = require('path');
 // const cookieParser = require("cookie-parser");
